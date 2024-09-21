2dc75f5a82574375f2c8596c78b42a427301d0dd
aandre.costaa <andre.costa@ipvc.pt>
2024-09-21 15:19:28 +0100

    Random Key generator script working

--- bet.js
+++ bet.js
@@ -1,4 +1,4 @@
-function generateRandomkey() {
+function generateRandomKey() {
     let numbers = [];
     let stars = [];
     while (numbers.length < 5) {
@@ -12,7 +12,14 @@ function generateRandomkey() {
         let randomStar = Math.floor(Math.random() * 12) + 1;
         if (!stars.includes(randomStar)) {
             stars.push(randomStar);
-            document.getElementById("s" + stars.length).innerHTML = randomStar;
+            document.getElementById("s" + stars.length).innerHTML =  randomStar;
         }
     }
+    console.log("Key generated!");
+    console.log("Numbers: " + numbers);
+    console.log("Stars: " + stars);
 }
+
+document.getElementById("generate").addEventListener("click", function() {
+    let randomNumbers = generateRandomKey();
+});
